c4c87f6f34d57f58a800970e536df35723f1e191
Subhajit Chakraborty <166530750+subhajit10000@users.noreply.github.com>
2026-07-02 10:45:50 +0530

    Enhance form styling and input attributes

--- Hook Form.jsx
+++ Hook Form.jsx
@@ -14,32 +14,57 @@ export default function App() {
     }
 
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
+        <form className=" text-center mt-40 bg-slate-500 pt-5 " onSubmit={handleSubmit(onSubmit)}>
             <div>
-                <label>First Name</label>
-                <input type="text" {...register("firstName", { required: "First name is required", maxLength: { value: 20, message: "Maximum length is 20" }, minLength: { value: 3, message: "Minimum length is 3" } })} />
+                <label className="text-xl">First Name</label>
+                <input type="text"
+                    className="bg-blue-100 border-2 ml-5"
+                    {...register("firstName",
+                        {
+                            required: "First name is required",
+                            maxLength: { value: 20, message: "Maximum length is 20" },
+                            minLength: { value: 3, message: "Minimum length is 3" }
+                        })}
+                    placeholder=" Enter Your First Name" />
                 {errors.firstName && <p>{errors.firstName.message}</p>}
             </div>
 
             <br />
 
             <div>
-                <label>Last Name</label>
-                <input type="text" {...register("lastName", { required: "Last name is required", maxLength: { value: 20, message: "Maximum length is 20" }, minLength: { value: 3, message: "Minimum length is 3" } })} />
+                <label className="text-xl ">Last Name</label>
+                <input type="text"
+                    className="bg-blue-100 border-2 ml-5 "
+                    {...register("lastName", {
+                        required: "Last name is required",
+                        maxLength: { value: 20, message: "Maximum length is 20" },
+                        minLength: { value: 3, message: "Minimum length is 3" }
+                    })}
+                    placeholder=" Enter Your Last Name" />
                 {errors.lastName && <p>{errors.lastName.message}</p>}
             </div>
 
             <br />
 
             <div>
-                <label>Age</label>
-                <input type="number"{...register("age", { required: "Age is required", valueAsNumber: true, min: { value: 18, message: "Minimum age is 18" }, max: { value: 99, message: "Maximum age is 99" } })} />
+                <label className="text-xl" >Age</label>
+                <input type="number"
+                    className="bg-blue-100 border-2 ml-5"
+                    {...register("age", {
+                        required: "Age is required",
+                        valueAsNumber: true,
+                        min: { value: 18, message: "Minimum age is 18" },
+                        max: { value: 99, message: "Maximum age is 99" }
+                    })}
+                    placeholder=" Enter Your Age" />
                 {errors.age && <p>{errors.age.message}</p>}
             </div>
 
             <br />
 
-            <input type="submit" />
+            <input type="submit"
+                className="text-2xl bg-green-600 border-2 m-5 p-5 rounded-xl hover:bg-blue-500 hover:text-2xl " />
         </form>
     );
 }
+
